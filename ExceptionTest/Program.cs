01f1c04efe77b35e222bfe011c7ef5f5c6dab3a6
Adam McCoy <adam.mccoy@octopus.com>
2024-08-15 12:56:21 +1000

    Initial commit

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+for (var i = 0; i < 1000; i++)
+{
+    try
+    {
+        throw new Exception();
+    }
+    catch (Exception)
+    {
+    }
+}
+Console.WriteLine($"Done in {sw.Elapsed}");
